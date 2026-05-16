cf4722b35fd44c4c81baeac304d6a05ab433676e
nexepic <remarkable.yh@gmail.com>
2026-05-16 18:47:10 +0800

    init commit

---
+++ Formula/zyx.rb
@@ -0,0 +1,32 @@
+class Zyx < Formula
+  desc "High-performance embeddable graph database with Cypher query support"
+  homepage "https://github.com/nexepic/zyx"
+  url "https://github.com/nexepic/zyx/archive/refs/tags/v0.1.7.tar.gz"
+  sha256 "bf6b36125c9f5486ce9f3d37b6ab2bed2e1e8f52de9548c460aea1423741db88"
+  license "Apache-2.0"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "boost"
+  depends_on "cli11"
+  depends_on "antlr4-cpp-runtime"
+
+  def install
+    mkdir "buildDir"
+    system "meson", "setup", "buildDir",
+           "--buildtype=release",
+           "-Ddefault_library=static",
+           "-Db_lto=true",
+           "-Dwarning_level=0",
+           "-Dtests=disabled",
+           "--prefix=#{prefix}",
+           "--libdir=lib"
+    system "meson", "compile", "-C", "buildDir"
+    bin.install "buildDir/apps/cli/zyx"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/zyx --version")
+  end
+end
